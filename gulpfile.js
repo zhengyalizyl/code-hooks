385656cf649dbd7fee64c730b82b46e0798ef1e2
lizheng <zyl863344241@qq.com>
2024-11-14 22:34:40 +0800

    修改样式

--- gulpfile.js
+++ gulpfile.js
@@ -10,25 +10,25 @@ const ts = require('gulp-typescript');
 const babel = require('gulp-babel');
 const del = require('del');
 
-//针对每次build时都执行delete操作
-gulp.task('clean', async function () {
+// 针对每次build时都执行delete操作
+gulp.task('clean', async () => {
   await del('lib/**');
   await del('es/**');
   await del('dist/**');
 });
 
-//生成ESM
-gulp.task('es', function () {
+// 生成ESM
+gulp.task('es', () => {
   const tsProject = ts.createProject('tsconfig.pro.json', {
     module: 'ESNext',
   });
 
-  //gulp的链式调用
+  // gulp的链式调用
   return tsProject.src().pipe(tsProject()).pipe(babel()).pipe(gulp.dest('es/'));
 });
 
-//生成cjs
-gulp.task('cjs', function () {
+// 生成cjs
+gulp.task('cjs', () => {
   return gulp
     .src(['./es/**/*.js'])
     .pipe(
@@ -39,16 +39,16 @@ gulp.task('cjs', function () {
     .pipe(gulp.dest('lib/'));
 });
 
-//生成声明文件
-gulp.task('declaration', function () {
+// 生成声明文件
+gulp.task('declaration', () => {
   const tsProject = ts.createProject('tsconfig.pro.json', {
-    declaration: true, //生成声明文件
-    emitDeclarationOnly: true, //只生成DTS文件
+    declaration: true, // 生成声明文件
+    emitDeclarationOnly: true, // 只生成DTS文件
   });
   return tsProject.src().pipe(tsProject()).pipe(gulp.dest('es/')).pipe(gulp.dest('lib/'));
 });
 
-gulp.task('copyReadme', async function () {
+gulp.task('copyReadme', async () => {
   await gulp.src('../../README.md').pipe(gulp.dest('../../packages/hooks'));
 });
 
