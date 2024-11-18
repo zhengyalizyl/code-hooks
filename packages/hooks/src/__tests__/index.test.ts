922cee5723e2e532fa70f0ba87fdc8990437f99b
lizheng <zyl863344241@qq.com>
2024-11-18 23:52:01 +0800

    add:新增

--- packages/hooks/src/__tests__/index.test.ts
+++
@@ -1,9 +0,0 @@
-import * as encodeHooks from '..';
-
-describe('encode hooks is defined', () => {
-  test('encode hooks should be defined', () => {
-    Object.keys(encodeHooks).forEach((module) => {
-      expect(encodeHooks[module]).toBeDefined;
-    });
-  });
-});
